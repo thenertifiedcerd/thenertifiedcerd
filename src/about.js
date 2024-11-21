f5e03da14cd329190513046a364eaac9f0ba496e
thenertifiedcerd <benzenefemiakinwande@gmail.com>
2024-11-21 01:50:13 +0100

    modified:   src/about.js

--- src/about.js
+++ src/about.js
@@ -16,10 +16,10 @@ function About() {
       <div className="About">
       <div className="">
         <div className="about-page">
-        <h2>About Us</h2>
+        <h3>About Us</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <br /><hr />
-        <h2>Our Mission</h2>
+        <h3>Our Mission</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <br /><hr />
